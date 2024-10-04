3c1c69a9114afda6fee8f82668294269f208d406
Mateus Nunes <mateusnss@protonmail.ch>
2024-10-04 18:11:15 -0300

    uwu

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,7 @@ function App() {
     <div style={{ padding: '20px', textAlign: 'center', background: '#ffdddd' }}>
       <h2>Aviso!</h2>
       <p>
-        ;-; O aplicativo não está otimizado para dispositivos móveis ou tablets em
-        modo retrato. Por favor, gire seu dispositivo ou acesse de um tablet em
+        ;-; Oops, vamos precisar de uma tela maior. Por favor, gire seu dispositivo ou acesse de um tablet em
         modo horizontal ou desktop. &#40;┬┬﹏┬┬&#41;
       </p>
     </div>
